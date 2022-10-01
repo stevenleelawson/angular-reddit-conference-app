00bebdc87afa5c904b26171db61d6bd24c77d8d7
Steven Lawson <steven.lawson@healthsqyre.com>
2022-10-01 12:38:35 -0600

    adding ngSwitch directive for early and late start

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,6 +7,11 @@ import { Component, Input } from "@angular/core";
 		<h2>{{event.name}}</h2>
 		<div>Date: {{event?.date}}</div>
 		<div>Time: {{event?.time}}</div>
+		<div [ngSwitch]="event?.time" [ngClass]="getStartTimeClass()">
+			<span *ngSwitchCase="'8:00 am'">Early Start</span>
+			<span *ngSwitchCase="'10:00 am'">Late Start</span>
+			<span *ngSwitchDefault>Normal Start</span>
+		</div>
 		<div>Price: \${{event?.price}}</div>
 		<div *ngIf="event?.location">
 			<span>Location: {{ event?.location.address }} </span>
@@ -18,12 +23,19 @@ import { Component, Input } from "@angular/core";
 	</div>
 	`,
 	styles: [`
-		.thumbnail { min-height: 210px }
+		.bold { font-weight: bold }
+		.green { color: #003300 !important }
+		.thumbnail { min-height: 255px }
 		.pad-left { margin-left: 10px }
 		.well div { color: #bbb }
 	`]
 })
 export class EventThumbnailComponent {
 	@Input() event:any
+
+	getStartTimeClass() {
+		const isEarlyStart = this.event && this.event.time === '8:00 am'
+		return { green: isEarlyStart, bold: isEarlyStart }
+	}
 }
 
