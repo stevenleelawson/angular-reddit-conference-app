c2d985cdcfef11cd8cb9f66eb2a0296a23916bff
Steven Lawson <steven.lawson@healthsqyre.com>
2022-10-01 13:24:32 -0600

    adding toastr popups messages and wrapping in a Service for 3rd party libraries

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { EventService } from "./shared/event.service";
+import { ToastrService } from '../common/toastr.service';
 
 @Component({
 	selector: 'events-list',
@@ -8,17 +9,21 @@ import { EventService } from "./shared/event.service";
 			<h1>Upcoming Angular Events</h1>
 			<hr>
 		<div class="col-md-5" *ngFor="let event of events">
-			<event-thumbnail [event]="event"></event-thumbnail>
+			<event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
 		<div>
 		</div>
 	`,
 })
-export class EventsListComponent {
+export class EventsListComponent implements OnInit {
 	events: any[];
-	constructor(private eventService: EventService) {
+	constructor(private eventService: EventService, private toastr: ToastrService) {
 	}
 	
 	ngOnInit() {
 		this.events = this.eventService.getEvents()
+	}
+
+	handleThumbnailClick(eventName) {
+		this.toastr.success(eventName);
 	}
 }
